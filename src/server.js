1964bf96d789c0f92fde2c671ca2000c8fbbad98
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-05-24 22:22:25 +0200

    yep

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import pino from 'pino';
 import pinoHttp from 'pino-http';
 import { env } from './utils/env.js';
+import { getContacts, getContactsById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -17,6 +18,39 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(pinoHttp({ logger }));
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getContacts();
+    res.status(200).json({
+      status: res.statusCode,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      res.status(404).json({
+        message: 'Problem exists with contact id!',
+      });
+      return;
+    }
+    const contact = await getContactsById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id ${contactId} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      status: res.statusCode,
+      message: `Successfully found contact with id: ${contactId}!`,
+      data: contact,
+    });
+  });
 
   app.use('*', (req, res) => {
     res.status(404).json({
