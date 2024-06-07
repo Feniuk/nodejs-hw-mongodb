dcea787bffc7d787e613a33d9bcbd69f9028e252
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-06-07 12:40:06 +0200

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,26 @@ import {
   patchSContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middleware/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', getContactsByIdController);
-router.post('/contacts', ctrlWrapper(createContactController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactByIdController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchSContactController));
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchSContactController),
+);
 
 export default router;
